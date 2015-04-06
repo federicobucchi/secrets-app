2aded94d6d022e99364f9635d6c3eca5b2c45164
Federico Bucchi <b.federico@gmail.com>
2015-04-06 12:05:56 -0700

    create secret model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405004607) do
+ActiveRecord::Schema.define(version: 20150406190334) do
+
+  create_table "secrets", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "text",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
