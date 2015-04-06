20cb2c32780b8c770354b9bb59fad6ba0c7fb7aa
Federico Bucchi <b.federico@gmail.com>
2015-04-06 12:21:38 -0700

    add relationship between User and Secrets

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406190334) do
+ActiveRecord::Schema.define(version: 20150406191724) do
 
   create_table "secrets", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.string   "text",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "user_id",    limit: 4
   end
+
+  add_index "secrets", ["user_id"], name: "index_secrets_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
